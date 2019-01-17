2807b92552e57d0af2e00acce79ef125b9c07208
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2019-01-17 10:37:06 +0100

    dioporcello

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ var alexa = require("alexa-app");
 var PORT = process.env.port || 8080;
 var app = express();
 
-var app_alexa = new alexa.app("test");
+var app_alexa = new alexa.app("ProvaSkill");
 
 app_alexa.express({
     expressApp: app,
     checkCert: false,
     debug: true
+});
+
+app.set("view engine", "ejs");
+
+alexaApp.launch(function(request, response) {
+  response.say("hai lanciato la prova");
 });
 
 app_alexa.intent("provaIntent", {
@@ -22,3 +28,4 @@ app_alexa.intent("provaIntent", {
 });
 
 app.listen(PORT);
+console.log("Listening on port " + PORT + ", try http://localhost:" + PORT + "/test");
